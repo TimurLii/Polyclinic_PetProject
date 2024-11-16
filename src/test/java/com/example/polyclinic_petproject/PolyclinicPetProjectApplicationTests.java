8848c60f66146d44c46d747e2a281a0fc5eb70d4
Timur <timurka260187@gmail.com>
2024-11-16 08:39:40 +0200

    first commit

---
+++ src/test/java/com/example/polyclinic_petproject/PolyclinicPetProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.polyclinic_petproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PolyclinicPetProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
