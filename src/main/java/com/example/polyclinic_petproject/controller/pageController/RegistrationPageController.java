556ad5c3d07eb1292c7e227138ab7f719d78c78d
Timur <timurka260187@gmail.com>
2024-11-17 16:20:22 +0200

    Setting up spring security. Works for now.

--- src/main/java/com/example/polyclinic_petproject/controller/pageController/RegistrationPageController.java
+++ src/main/java/com/example/polyclinic_petproject/controller/pageController/RegistrationPageController.java
@@ -12,19 +12,22 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/registration")
+@RequestMapping("/")
 public class RegistrationPageController {
     @Autowired
     private PatientService patientService;
 
     @GetMapping()
     public String showRegistrationPage(Model model) { // Добавьте параметр Model
+        System.out.println("1");
         model.addAttribute("patient", new Patient()); // Добавьте новый объект Patient в модель
         return "registrationFormPage"; // Возврат шаблона
     }
 
     @PostMapping()
     public String registerPatient(@ModelAttribute Patient patient) {
+        System.out.println("2");
+
         if (patient != null) {
             patientService.savePatient(patient);
             System.out.println(patient);
